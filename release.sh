00f483fb28261e2a0b0285a5423a6543c62573a3
Christos Georgiou <christos@cgeosoft.com>
2023-01-27 11:57:18 +0200

    Release v1.0.1

---
+++ release.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+OLD_VERSION=$(cat package.json \
+  | grep version \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g')
+
+NEW_VERSION=$(echo $OLD_VERSION | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{if(length($NF+1)>length($NF))$(NF-1)++; $NF=sprintf("%0*d", length($NF), ($NF+1)%(10^length($NF))); print}')
+
+npm version $NEW_VERSION
+
+git add .
+git commit -m "Release v$NEW_VERSION"
+git push
+
+git tag v$NEW_VERSION main
+git push --tags
