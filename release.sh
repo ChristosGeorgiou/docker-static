b2328e9552b1d29eab99848b4b771a0497cffde7
Christos Georgiou <christos@cgeosoft.com>
2023-01-27 12:00:53 +0200

    Release v1.0.5

--- release.sh
+++ release.sh
@@ -8,8 +8,13 @@ OLD_VERSION=$(cat package.json \
 
 NEW_VERSION=$(echo $OLD_VERSION | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{if(length($NF+1)>length($NF))$(NF-1)++; $NF=sprintf("%0*d", length($NF), ($NF+1)%(10^length($NF))); print}')
 
+git add .
+
 npm version $NEW_VERSION
 
 git add .
 git commit -m "Release v$NEW_VERSION"
 git push
+
+git tag v$NEW_VERSION main
+git push --tags
