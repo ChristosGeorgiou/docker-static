05b48e95901ea0c5daabc268f86a93dd7afc7322
Christos Georgiou <christos@cgeosoft.com>
2023-10-02 17:02:29 +0300

    ...

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,24 @@
-FROM node:alpine
+FROM node:18-alpine
 
-COPY . /app
+RUN npm i -g pnpm
+
 WORKDIR /app
-
-ENV NODE_ENV production
-
-RUN npm install
 
 EXPOSE 3000
 
-CMD [ "npm", "start" ]
+COPY package*.json ./
+COPY pnpm-lock.yaml ./
+
+RUN pnpm install
+
+COPY . .
+
+RUN pnpm run build
+
+ENV NODE_ENV=production
+ENV APP_DIR=./app
+
+# HEALTHCHECK --interval=10s --timeout=10s --start-period=10s \
+#   CMD node dist/healthcheck.js http://localhost:3000/health/check
+
+CMD ["pnpm", "run", "start:prod"]
--- index.js
+++ index.js
@@ -1,39 +1,22 @@
 const express = require("express");
 const helmet = require("helmet");
 const dotenv = require("dotenv");
-const expressHandlebars = require("express-handlebars");
-const morgan = require("morgan");
-const fs = require("fs");
-const path = require("path");
 
 dotenv.config();
 
 const app = express();
 const port = 3000;
 
-app.engine(
-  ".hbs",
-  expressHandlebars({
-    extname: ".hbs",
-  })
-);
-
-app.set("view engine", ".hbs");
-
-const logsDir = path.join(__dirname, "logs");
-fs.mkdirSync(logsDir, { recursive: true });
-var accessLogStream = fs.createWriteStream(path.join(logsDir, "access.log"), {
-  flags: "a",
-});
+app.set('view engine', 'pug')
 
 app.use(helmet());
 
-app.use(morgan("combined", { stream: accessLogStream }));
-
+app.use(express.static("static"));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.render("index", { env: process.env });
+  console.log(`[${req.ip}] ${req.get("User-Agent")}`);
+  res.render("index", { ...process.env });
 });
 
 app.listen(port, () =>
